cbb47be58dc351b5158128b55cdeb9da625d2058
Sourabh112 <sourabhchoudhary112112@gmail.com>
2023-06-19 15:41:58 +0530

    My first java Project

---
+++ src/com/Flipkart/demo/DemoTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.Flipkart.demo;
+
+/**
+ * 
+ */
+public class DemoTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
